0cb274fbe0b90e5107d0b4cc5f4f24224d74cee7
Probir Sarkar <probir.sarkar.agt@gmail.com>
2025-04-22 12:53:48 +0530

    register successfully

--- src/api/endpoints/auth.ts
+++ src/api/endpoints/auth.ts
@@ -1,1 +1,11 @@
 import instance from "../axios";
+import type { FormValues as RegisterFormValues } from "@/components/auth/register-form";
+
+type RegisterResponse = {
+  message: string;
+  token: string;
+};
+export async function register(data: RegisterFormValues) {
+  const response = await instance.post<RegisterResponse>("/register", data);
+  return response.data;
+}
--- src/routes/register/index.tsx
+++ src/routes/register/index.tsx
@@ -1,14 +1,14 @@
-import { LoginForm } from "@/components/auth/login-form";
+import { RegisterForm } from "@/components/auth/register-form";
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/login/")({
+export const Route = createFileRoute("/register/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
     <div className="max-w-md w-full mx-auto my-12">
-      <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
